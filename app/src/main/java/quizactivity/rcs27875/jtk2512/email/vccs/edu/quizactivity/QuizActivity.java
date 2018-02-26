73d8f38542304ba2a83080dc2c166a6bf6a005c7
rcs27875 <rcs27875@email.vccs.edu>
2018-02-26 12:14:38 -0500

    Properly fixed rotation

--- app/src/main/java/quizactivity/rcs27875/jtk2512/email/vccs/edu/quizactivity/QuizActivity.java
+++ app/src/main/java/quizactivity/rcs27875/jtk2512/email/vccs/edu/quizactivity/QuizActivity.java
@@ -1,7 +1,9 @@
 package quizactivity.rcs27875.jtk2512.email.vccs.edu.quizactivity;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    static final String CURRENT_INDEX = "currentIndex";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -25,12 +29,20 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true)
     };
 
-    private static int mCurrentIndex = 0;
+    private static int mCurrentIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null)
+            mCurrentIndex = savedInstanceState.getInt(CURRENT_INDEX);
+        else
+            mCurrentIndex = 0;
+
+
         setContentView(R.layout.activity_quiz);
+        Log.d(TAG, "onCreate...");
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
@@ -63,6 +75,65 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        Log.d(TAG,"onStart...");
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        Log.d(TAG, "onResume...");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        Log.d(TAG, "onPause...");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+
+        Log.d(TAG, "onStop...");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy...");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+
+        Log.d(TAG, "onRestart...");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putInt(CURRENT_INDEX, mCurrentIndex);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+
+        Log.d(TAG, "Saving state...");
     }
 
     private void updateQuestion()
