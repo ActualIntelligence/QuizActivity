4bf00d4d03df37ac453d2c9fd4a40fb6fd818c2c
jtk2512 <jtk2512@email.vccs.edu>
2018-02-26 12:15:21 -0500

    changes as done in class on the 26th

--- app/src/main/java/quizactivity/rcs27875/jtk2512/email/vccs/edu/quizactivity/QuizActivity.java
+++ app/src/main/java/quizactivity/rcs27875/jtk2512/email/vccs/edu/quizactivity/QuizActivity.java
@@ -2,12 +2,15 @@ package quizactivity.rcs27875.jtk2512.email.vccs.edu.quizactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String INDEX = "currentIndex";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -28,6 +31,14 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(INDEX);
+        }
+        else {
+            mCurrentIndex = 0;
+        }
+
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -61,6 +72,7 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        Log.d(TAG,"onCreate");
     }
 
     private void updateQuestion(){
@@ -82,4 +94,47 @@ public class QuizActivity extends AppCompatActivity {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(INDEX,mCurrentIndex);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
 }
